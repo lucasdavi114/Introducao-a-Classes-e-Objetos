b8d5808982be4e3232900ca59ee7291cca4323ce
Lucas Davi Ribeiro <lucasdaviribeiro46@gmail.com>
2026-09-20 16:27:45 -0300

    WIP

--- ascii/src/ascii.cpp
+++ ascii/src/ascii.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <clocale>
+#include <cstdlib>
 #include "ascii.h"
+
+int main()
+{
+    setlocale(LC_ALL, "pt-BR.UTF-8");
+    char valor = 0;
+    std::cout << valor;
+
+    for (int indice = 32; indice < 255; indice++)
+    {
+        std::cout << indice << ": " << static_cast<char>(indice) << "\n";
+    }
+
+    return 0;
+}
